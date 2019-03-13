f13e4e16287e2c93a87d5126c3d35678d590cbd1
Douglas da Costa Pinheiro <douglaspinheiroti@gmail.com>
2019-03-13 11:43:02 -0300

    change svm to ksvm

--- script.R
+++ script.R
@@ -1,22 +1,24 @@
 #pre processamento
 #ler base
 #dividir em treino e teste
-#criar o modelo (rodar o svm)
+#criar o modelo (rodar o ksvm)
 #analisar os resultados
 
 
-# install.packages('caTools')
+#install.packages('caTools')
 library(caTools)
-# install.packages('e1071')
+#install.packages('e1071')
 library(e1071)
-# install.packages('caret')
+#install.packages('caret')
 library(caret)
+#install.packages("kernlab")
+library(kernlab)
 
 
-#carregar a base na variável
+#carregar a base na variÃ¡vel
 base = read.csv2('pesquisa-artigo.csv')
 
-#encode (todos os atributos categóricos)
+#encode (todos os atributos categÃ³ricos)
 base$Timestamp <- NULL
 base_processed = base
 base_processed$Questao.01 = factor(base_processed$Questao.01, levels = c(1,2,3,4))
@@ -38,28 +40,11 @@ divisao = sample.split(base_processed$Questao.11, SplitRatio=0.8)
 base_treino = subset(base_processed, divisao == TRUE)
 base_teste = subset(base_processed, divisao == FALSE)
 
-kernels = c("linear", "radial", "polynomial", "sigmoid")
-best_accuracy = 0
-best_kernel = ""
-best_cost = 0
+prev_ksvm = predict(classif_ksvm, newdata = base_teste[-11])
 
-for(k in 1: 4) {
-  for (i in 1:10){
-    #classificador svm
-    classif_svm = svm(formula = Questao.11 ~ ., data = base_treino, type = 'C-classification', kernel = kernels[k], cost = i)
-    prev_svm = predict(classif_svm, newdata = base_teste[-11])
+matriz_confusao = table(base_teste[ ,11], prev_ksvm)
+result_cm = confusionMatrix(matriz_confusao)
+resumo_cm = result_cm$overall
+acuracia_cm = resumo_cm['Accuracy']
 
-    matriz_confusao = table(base_teste[ ,11], prev_svm)
-    result_cm = confusionMatrix(matriz_confusao)
-    resumo_cm = result_cm$overall
-    acuracia_cm = resumo_cm['Accuracy']
-    
-    if(acuracia_cm > best_accuracy){
-      best_accuracy = acuracia_cm;
-      best_kernel = kernels[k]
-      best_cost = i
-    }
-  }
-}
-
-paste("A melhor acurácia foi de", best_accuracy, " com o kernel", best_kernel, "e com o custo", best_cost)
+paste("A melhor acurÃ¡cia foi de", acuracia_cm)
